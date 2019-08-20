c7b0123ebb749fc97bc0cadeeda4eb0ea2150a69
susanliubc <susanliubc@gmail.com>
2019-08-19 19:45:46 -0700

    add routing foler

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/AuthContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authContext = useContext(AuthContext);
+
+  const { loading, isAuthenticated } = authContext;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
